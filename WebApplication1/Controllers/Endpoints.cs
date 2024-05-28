c8371fab029cdde11f1431167b60b2f35f2633b3
thon1525 <thonhourn1525@gmail.com>
2024-05-28 14:37:29 +0700

    add simple web

---
+++ WebApplication1/Controllers/Endpoints.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+namespace WebApplication1.Controllers;
+
+public static class Endpoints
+{
+    public static void MapHelloControllerEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/HelloController").WithTags(nameof(HelloController));
+
+        group.MapGet("/", () =>
+        {
+            return new [] { new HelloController() };
+        })
+        .WithName("GetAllHelloControllers")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            //return new HelloController { ID = id };
+        })
+        .WithName("GetHelloControllerById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", (int id, HelloController input) =>
+        {
+            return TypedResults.NoContent();
+        })
+        .WithName("UpdateHelloController")
+        .WithOpenApi();
+
+        group.MapPost("/", (HelloController model) =>
+        {
+            //return TypedResults.Created($"/api/HelloControllers/{model.ID}", model);
+        })
+        .WithName("CreateHelloController")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            //return TypedResults.Ok(new HelloController { ID = id });
+        })
+        .WithName("DeleteHelloController")
+        .WithOpenApi();
+    }
+}
